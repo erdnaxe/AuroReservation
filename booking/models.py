89f8b330a1b87f24c72b98caadca4df75904a098
pa <crepon.pierre.andre@gmail.com>
2019-03-07 19:05:40 +0100

    First commit
      - Setup the project
      - Filled the models.py

---
+++ booking/models.py
@@ -0,0 +1,67 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class Room(models.Model):
+    name = models.CharField(
+        verbose_name=_('name'),
+        max_length=255,
+    )
+    comment = models.TextField(
+        verbose_name=_('comment'),
+    )
+    manager = models.ForeignKey(
+        'User',
+        verbose_name=_('manager'),
+    )
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = _('room')
+        verbose_name_plural = _('rooms')
+
+    def __str__(self):
+        return self.name
+
+
+class Reservation(models.Model):
+    start_time = models.DateTimeField(
+        verbose_name=_('start_time'),
+    )
+    end_time = models.DateTimeField(
+        verbose_name=_('end_time'),
+    )
+    room = models.ForeignKey(
+        'Room',
+        verbose_name=_('room'),
+        on_delete=models.PROTECT,
+    )
+    number_participants = models.IntegerFields(
+        verbose_name=_('number of participants'),    
+    )
+    validation = models.BooleanField(
+        verbose_name=_('validation'),
+        null=True
+    )
+    purpose_title = models.CharField(
+        verbose_name=_('title'),
+        max_length=255),
+    )
+    purpose_body = models.TextField(
+        verbose_name=_('purpose'),
+    )
+    in_charges = models.ManyToManyField(
+        'User',
+        verbose_name=_('in charge'),
+        verbose_name_plural=_('in charges'),
+    )
+
+    class Meta:
+        ordering = ['start_time']
+        verbose_name = _('reservation')
+        verbose_name_plural = _('reservations')
+
+    def __str__(self):
+        return str(start_time) + " -> " + str(end_time) + " : " + purpose_title
+
