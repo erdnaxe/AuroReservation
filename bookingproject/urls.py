79cffc549bfd75ae55b847795392172fdbf0bc30
Alexandre Iooss <erdnaxe@crans.org>
2019-04-30 19:42:45 +0200

    Move admin to root

--- bookingproject/urls.py
+++ bookingproject/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^i18n/', include('django.conf.urls.i18n')),
-    url(r'^admin/', admin.site.urls),
+    url(r'^', admin.site.urls),
 ]
