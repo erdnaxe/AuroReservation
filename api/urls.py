4e78ab5372579e3840ba446aeab3080ed332d683
Alexandre Iooss <erdnaxe@crans.org>
2019-05-07 12:35:01 +0200

    Add booking API

--- api/urls.py
+++ api/urls.py
@@ -3,10 +3,18 @@
 
 from rest_framework import routers
 
+from booking.views import TagViewSet, RoomViewSet, ReservationViewSet
 from users.views import UserViewSet, GroupViewSet
 
 router = routers.DefaultRouter()
+
+# users app
 router.register(r'users', UserViewSet)
 router.register(r'groups', GroupViewSet)
+
+# booking app
+router.register(r'tags', TagViewSet)
+router.register(r'rooms', RoomViewSet)
+router.register(r'reservations', ReservationViewSet)
 
 urlpatterns = router.urls
--- booking/views.py
+++ booking/views.py
@@ -1,10 +1,21 @@
+# -*- mode: python; coding: utf-8 -*-
+# SPDX-License-Identifier: GPL-2.0-or-later
+
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.utils.translation import gettext_lazy as _
+from rest_framework import viewsets
+from rest_framework.permissions import DjangoModelPermissions
+
+from .models import Tag, Room, Reservation
+from .serializers import TagSerializer, RoomSerializer, ReservationSerializer
 
 
 @login_required
 def index(request):
+    """
+    Index view
+    """
     context = {
         'title': _('Home'),
     }
@@ -14,8 +25,40 @@ def index(request):
 
 @login_required
 def profile(request):
+    """
+    User profile view
+    """
     context = {
         'title': _('My profile'),
     }
 
     return render(request, 'booking/profile.html', context=context)
+
+
+class TagViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows tags to be viewed or edited.
+    """
+    permission_classes = (DjangoModelPermissions,)
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+
+class RoomViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows rooms to be viewed or edited.
+    """
+    permission_classes = (DjangoModelPermissions,)
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+
+
+class ReservationViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    API endpoint that allows reservations to be viewed or edited.
+
+    It is read-only for security reasons.
+    """
+    permission_classes = (DjangoModelPermissions,)
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
