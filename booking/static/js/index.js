f14e96c4f81f7d42512713f15a20379998aacd4a
Alexandre Iooss <erdnaxe@crans.org>
2019-06-17 07:44:39 +0200

    Change quote to doublequote in JS

--- booking/static/js/index.js
+++ booking/static/js/index.js
@@ -1,42 +1,42 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
-import {Calendar} from '@fullcalendar/core';
-import resourceTimelinePlugin from '@fullcalendar/resource-timeline';
-import '@fullcalendar/core/main.css';
-import '@fullcalendar/timeline/main.css';
-import '@fullcalendar/resource-timeline/main.css';
-import frLocale from '@fullcalendar/core/locales/fr';
-import esLocale from '@fullcalendar/core/locales/es';
-import tippy from 'tippy.js'
-import '../css/fullcalendar.css';
+import {Calendar} from "@fullcalendar/core";
+import resourceTimelinePlugin from "@fullcalendar/resource-timeline";
+import "@fullcalendar/core/main.css";
+import "@fullcalendar/timeline/main.css";
+import "@fullcalendar/resource-timeline/main.css";
+import frLocale from "@fullcalendar/core/locales/fr";
+import esLocale from "@fullcalendar/core/locales/es";
+import tippy from "tippy.js";
+import "../css/fullcalendar.css";
 
-document.addEventListener('DOMContentLoaded', function () {
-    let calendarEl = document.getElementById('calendar');
+document.addEventListener("DOMContentLoaded", function () {
+    let calendarEl = document.getElementById("calendar");
     let calendar = new Calendar(calendarEl, {
-        schedulerLicenseKey: 'GPL-My-Project-Is-Open-Source',
+        schedulerLicenseKey: "GPL-My-Project-Is-Open-Source",
         plugins: [resourceTimelinePlugin],
-        timeZone: 'Europe/Paris',
-        defaultView: 'resourceTimelineWeek',
-        resourceLabelText: '\ ',
+        timeZone: "Europe/Paris",
+        defaultView: "resourceTimelineWeek",
+        resourceLabelText: "\ ",
         nowIndicator: true,
-        slotLabelInterval: '04:00:00',
+        slotLabelInterval: "04:00:00",
         resourceAreaWidth: "20%",
         header: {
-            left: '',
-            center: 'title',
-            right: 'today prev,next resourceTimelineDay,resourceTimelineWeek'
+            left: "",
+            center: "title",
+            right: "today prev,next resourceTimelineDay,resourceTimelineWeek"
         },
         height: "auto",
         locales: [frLocale, esLocale],
-        resources: 'fc/resources.json',
-        events: 'fc/events.json',
+        resources: "fc/resources.json",
+        events: "fc/events.json",
         resourceRender: function (renderInfo) {
-            const cellText = renderInfo.el.querySelector('.fc-cell-text');
+            const cellText = renderInfo.el.querySelector(".fc-cell-text");
 
             // Link to add a reservation
             if (renderInfo.resource.extendedProps.add_url) {
-                cellText.classList.add('link');
-                cellText.addEventListener('click', function () {
+                cellText.classList.add("link");
+                cellText.addEventListener("click", function () {
                     window.location.href = renderInfo.resource.extendedProps.add_url;
                 });
             }
@@ -45,21 +45,21 @@ document.addEventListener('DOMContentLoaded', function () {
             if (renderInfo.resource.extendedProps.comment) {
                 new tippy(cellText, {
                     content: renderInfo.resource.extendedProps.comment.replace(/\n/g, "<br />"),
-                    placement: 'right',
+                    placement: "right",
                     arrow: true,
-                    boundary: 'window',
+                    boundary: "window",
                 });
             }
         },
         eventRender: function (renderInfo) {
-            renderInfo.el.style.color = 'white';
-            const cellText = renderInfo.el.querySelector('.fc-content');
+            renderInfo.el.style.color = "white";
+            const cellText = renderInfo.el.querySelector(".fc-content");
 
             // Tooltip
             if (renderInfo.event.extendedProps.comment) {
                 new tippy(renderInfo.el, {
                     content: renderInfo.event.extendedProps.comment,
-                    placement: 'top',
+                    placement: "top",
                     arrow: true,
                 });
             }
@@ -69,12 +69,12 @@ document.addEventListener('DOMContentLoaded', function () {
     calendar.render();
 
     // Switch locale with Django locale
-    calendar.setOption('locale', document.documentElement.lang);
+    calendar.setOption("locale", document.documentElement.lang);
 
     // Scroll to now
     setTimeout(function () {
-        let today = document.querySelector('.fc-now-indicator');
-        let scroller = document.getElementsByClassName('fc-scroller')[1];
+        let today = document.querySelector(".fc-now-indicator");
+        let scroller = document.getElementsByClassName("fc-scroller")[1];
         scroller.scrollLeft = today.offsetLeft;
     }, 10);
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,24 @@
 const path = require("path");
-const BundleTracker = require('webpack-bundle-tracker');
+const BundleTracker = require("webpack-bundle-tracker");
 
 module.exports = {
-    mode: 'development',
-    entry: './booking/static/js/index',
+    mode: "development",
+    entry: "./booking/static/js/index",
     resolve: {
-        extensions: ['.js']
+        extensions: [".js"]
     },
     output: {
-        path: path.resolve('./booking/static/bundles/'),
+        path: path.resolve("./booking/static/bundles/"),
         filename: "[name]-[hash].js",
     },
     plugins: [
-        new BundleTracker({filename: './webpack-stats.json'}),
+        new BundleTracker({filename: "./webpack-stats.json"}),
     ],
     module: {
         rules: [
             {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
+                use: ["style-loader", "css-loader"],
             },
         ],
     }
